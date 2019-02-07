f214c9ca4efc0b3be328cd201605df8ecd1df906
Charley <diamonddynamo96@gmail.com>
2019-02-07 13:12:26 -0500

    Clock display final

--- Assign02/Main.cpp
+++ Assign02/Main.cpp
@@ -1,17 +1,15 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int main() {
-	const int SIZE = 20;
-	
-	char clock[SIZE][SIZE];
 
-	int half = SIZE / 2;
-	int onethird = SIZE / 3;
-	int twothird = (SIZE / 3) * 2;
-	int onesixth = SIZE / 6;
-	int fivesixth = (SIZE / 6) * 5;
+int main() {
+	const int SIZE = 50;
+	int N = SIZE / 2;
+
+	char clock[SIZE][SIZE];
 
 	for (int i = 0; i < SIZE; i++) {
 		for (int j = 0; j < SIZE; j++) {
@@ -19,68 +17,34 @@ int main() {
 		}
 	}
 
+	clock[N][N] = 'x';
+	
+	for (int i = 1; i <= 12; i++) {
+		float angle = i * (M_PI / 6);
+		int xpos = -(N * cos(angle)) + N;
+		if (xpos >= SIZE) { xpos = SIZE - 1; }
+		int ypos = N * sin(angle) + N;
+		if (ypos >= SIZE) { ypos = SIZE - 1; }
+		xpos = xpos;
+		ypos = ypos;
+		int outName = i % 10 + 48;
+		clock[xpos][ypos] = outName;
+		if (i > 9) {
+			clock[xpos][ypos - 1] = '1';
+		}
+	}
+	cout << endl;
 	for (int i = 0; i < SIZE; i++) {
 		for (int j = 0; j < SIZE; j++) {
-			if (i == 0) {
-				if (j >= half && j <= half) {
-					clock[i][j] = '1';
-					clock[i][j + 1] = '2';
-				}
-			}
-			else if (i <= onesixth && i >= onesixth) {
-				if (j <= onethird && j >= onethird) {
-					clock[i][j] = '1';
-					clock[i][j + 1] = '1';
-				}
-				if (j <= twothird && j >= twothird) {
-					clock[i][j] = '1';
-				}
-			}
-			else if (i <= onethird && i >= onethird) {
-				if (j <= onesixth && j >= onesixth) {
-					clock[i][j] = '1';
-					clock[i][j + 1] = '0';
-				}
-				if (j <= fivesixth && j >= fivesixth){
-					clock[i][j] = '2';
-				}
-			}
-			else if (i <= half && i >= half) {
-				if (j == 0) {
-					clock[i][j] = '9';
-				}
-				if (j == SIZE - 1) {
-					clock[i][j] = '3';
-				}
-				if (j <= half && j >= half) {
-					clock[i][j] = '0';
-				}
-			}
-			else if (i <= twothird && i >= twothird) {
-				if (j <= onesixth && j >= onesixth) {
-					clock[i][j] = '8';
-				}
-				if (j <= fivesixth && j >= fivesixth) {
-					clock[i][j] = '4';
-				}
-			}
-			else if (i <= fivesixth && i >= fivesixth) {
-				if (j <= onethird && j >= onethird) {
-					clock[i][j] = '7';
-				}
-				if (j <= twothird && j >= twothird) {
-					clock[i][j] = '5';
-				}
-			}
-			else if (i == SIZE - 1) {
-				if (j <= half && j >= half) {
-					clock[i][j] = '6';
-				}
-			}
 			cout << clock[i][j];
+			if (clock[i][j + 1] == ' ') {
+				cout << " ";
+			}
 		}
 		cout << endl;
 	}
 	cout << "done";
+	char temp;
+	cin >> temp;
 	return 0;
 }
